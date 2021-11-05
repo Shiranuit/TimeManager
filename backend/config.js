fc9fbfce35e1008ae4d723c952e6e525f8e9a6c4
Shiranuit <shiranui007@gmail.com>
2021-11-05 21:30:37 +0100

    support salt, cors, and permissions

--- backend/config.js
+++ backend/config.js
@@ -5,6 +5,12 @@ module.exports = {
   http: {
     host: 'localhost',
     port: 4000,
+    cors: {
+      'Access-Control-Allow-Origin': [
+        /^(http(s)?:\/\/)?tungsten\.ovh/,
+        /^(http(s)?:\/\/)?localhost/,
+      ],
+    },
   },
   auth: {
     maxLoginAttempts: 3,  // Max attemps per seconds
@@ -31,5 +37,78 @@ module.exports = {
     database: process.env.DB_NAME || 'postgres',
     user: process.env.DB_USER || 'postgres',
     password: process.env.DB_USER_PASSWORD || 'postgres',
+  },
+  permissions: {
+    anonymous: {
+      auth: {
+        login: true,
+        logout: true,
+        checkToken: true,
+        register: true,
+      }
+    },
+    user: {
+      auth: {
+        login: true,
+        logout: true,
+        checkToken: true,
+        register: true,
+
+        getMyUser: true,
+        updateMyUser: true,
+        deleteMyUser: true,
+      },
+      clock: {
+        getMyClock: true,
+        createOrUpdateMyClock: true,
+        deleteMyClock: true,
+      },
+      workingtime: {
+        listMyWorkingTimes: true,
+        createOrUpdateMyWorkingTime: true,
+        deleteMyWorkingTime: true,
+        updateMyWorkingTime: true,
+        getMyWorkingTime: true,
+      }
+    },
+    manager: {
+      auth: {
+        login: true,
+        logout: true,
+        checkToken: true,
+        register: true,
+
+        getMyUser: true,
+        updateMyUser: true,
+        deleteMyUser: true,
+      },
+      clock: {
+        getMyClock: true,
+        createOrUpdateMyClock: true,
+        deleteMyClock: true,
+
+        getClock: true,
+        createOrUpdateClock: true,
+        deleteClock: true,
+      },
+      workingtime: {
+        listMyWorkingTimes: true,
+        createMyWorkingTime: true,
+        deleteMyWorkingTime: true,
+        updateMyWorkingTime: true,
+        getMyWorkingTime: true,
+
+        listWorkingTimes: true,
+        createWorkingTime: true,
+        deleteWorkingTime: true,
+        updateWorkingTime: true,
+        getWorkingTime: true,
+      }
+    },
+    'super-manager': {
+      '*': {
+        '*': true,
+      }
+    }
   }
 };
