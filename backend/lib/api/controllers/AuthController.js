77eee3dac306e6b00d2765cba362651ef9052629
Shiranuit <shiranui007@gmail.com>
2021-11-09 18:14:06 +0100

    finalize AuthController testing

--- backend/lib/api/controllers/AuthController.js
+++ backend/lib/api/controllers/AuthController.js
@@ -99,7 +99,7 @@ class AuthController extends BaseController {
       error.throwError('security:user:password_too_short', this.config.password.minLength);
     }
 
-    if (!CAPITAL_PATTERN.test(password) || !LOWER_PATTERN.test(password) || !NUMBER_PATTERN.test(password)) {
+    if (!this._validatePasswordStrength(password)) {
       error.throwError('security:user:password_too_weak');
     }
 
@@ -205,7 +205,7 @@ class AuthController extends BaseController {
 
     // Should never happen but just in case
     if (!userInfos) {
-      error.throwError('security:user:not_found', req.getUser().id);
+      error.throwError('security:user:with_id_not_found', req.getUser().id);
     }
 
     const authorized = await this.backend.ask(
@@ -280,13 +280,6 @@ class AuthController extends BaseController {
     const password = req.getBodyString('oldPassword');
     const newPassword = req.getBodyString('newPassword');
 
-    const userInfos = await this.backend.ask('core:security:user:get', req.getUser().id);
-
-    // Should never happen but just in case
-    if (!userInfos) {
-      error.throwError('security:user:not_found', req.getUser().id);
-    }
-
     const authorized = await this.backend.ask(
       'core:security:user:verifyById',
       {
@@ -299,7 +292,7 @@ class AuthController extends BaseController {
       error.throwError('security:user:invalid_credentials');
     }
 
-    if (!CAPITAL_PATTERN.test(newPassword) || !LOWER_PATTERN.test(newPassword) || !NUMBER_PATTERN.test(newPassword)) {
+    if (!this._validatePasswordStrength(newPassword)) {
       error.throwError('security:user:password_too_weak');
     }
 
@@ -329,6 +322,18 @@ class AuthController extends BaseController {
     }
 
     return await this.backend.ask('core:security:user:delete', req.getUser().id);
+  }
+
+  /**
+   * Verifies that the password is strong enough
+   * 
+   * @param {string} password 
+   * @returns 
+   */
+  _validatePasswordStrength(password) {
+    return CAPITAL_PATTERN.test(password)
+      && LOWER_PATTERN.test(password)
+      && NUMBER_PATTERN.test(password);
   }
 }
 
--- backend/test/api/controllers/AuthController.test.js
+++ backend/test/api/controllers/AuthController.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const should = require('should');
+const sinon = require('sinon');
 const { AuthController } = require('../../../lib/api/controllers');
 const BackendMock = require('../../mocks/backend.mock');
 const Request = require('../../../lib/api/requests/request');
@@ -166,29 +167,14 @@ describe('AuthController', () => {
       );
     });
 
-    it('should reject if the password has no UpperCase letters', async () => {
-      request.input.body.password = 'abcdefgh';
+    it('should reject if the password is too weak', async () => {
+      authController._validatePasswordStrength = sinon.stub().returns(false);
       await should(authController.register(request)).be.rejectedWith(
         SecurityError,
         { id: 'security:user:password_too_weak' }
       );
     });
 
-    it('should reject if the password has no LowerCase letters', async () => {
-      request.input.body.password = 'ABCDEFGH';
-      await should(authController.register(request)).be.rejectedWith(
-        SecurityError,
-        { id: 'security:user:password_too_weak' }
-      );
-    });
-
-    it('should reject if the password has no Numbers', async () => {
-      request.input.body.password = 'Abcdefgh';
-      await should(authController.register(request)).be.rejectedWith(
-        SecurityError,
-        { id: 'security:user:password_too_weak' }
-      );
-    });
 
     it('should reject if cannot create the user', async () => {
       backend.ask.withArgs('core:security:user:create').resolves(null);
@@ -321,8 +307,450 @@ describe('AuthController', () => {
       });
     });
 
-    it('should reject if the user is not authenticated', () => {
-      
+    it('should reject if the user is not authenticated', async () => {
+      await should(authController.getMyUser(request)).be.rejectedWith(
+        SecurityError,
+        { id: 'security:user:not_authenticated' }
+      );
+    });
+
+    it('should ask core:security:user:get with the current user id', async () => {
+      request.context.user = { id: 1 };
+      backend.ask.withArgs('core:security:user:get')
+        .resolves({
+          id: 1,
+          username: 'foo',
+          email: 'bar',
+          role: 'user',
+        });
+
+      const response = await authController.getMyUser(request);
+
+      await should(backend.ask).be.calledWith('core:security:user:get', 1);
+
+      await should(response).be.an.Object()
+        .match({
+          id: 1,
+          username: 'foo',
+          email: 'bar',
+          role: 'user',
+        });
+    });
+  });
+
+  describe('#updateMyUser', () => {
+    let request;
+    beforeEach(() => {
+      request = new Request({
+        url: '',
+        headers: {},
+        host: '',
+      });
+      request.context.user = { id: 42 };
+      request.input.body = {
+        actualPassword: 'password',
+        username: 'USERNAME',
+        email: 'EMAIL@EMAIL.COM',
+        role: 'ROLE',
+      };
+    });
+
+    it('should reject if the user is not authenticated', async () => {
+      request.context.user = { id: null };
+      await should(authController.updateMyUser(request)).be.rejectedWith(
+        SecurityError,
+        { id: 'security:user:not_authenticated' }
+      );
+    });
+
+    it('should reject if the current user is not found', async () => {
+      backend.ask.withArgs('core:security:user:get').resolves(null);
+      await should(authController.updateMyUser(request)).be.rejectedWith(
+        SecurityError,
+        { id: 'security:user:with_id_not_found' }
+      );
+    });
+
+    it('should reject if the given password does not match the user\'s password', async () => {
+      backend.ask.withArgs('core:security:user:get')
+        .resolves({
+          id: 42,
+          username: 'foo',
+          email: 'bar',
+          role: 'user'
+        });
+
+      backend.ask.withArgs('core:security:user:verifyById')
+        .resolves(false);
+
+      await should(authController.updateMyUser(request)).be.rejectedWith(
+        SecurityError,
+        { id: 'security:user:invalid_credentials' }
+      );
+
+      await should(backend.ask).be.calledWith('core:security:user:get', 42);
+      await should(backend.ask).be.calledWith(
+        'core:security:user:verifyById',
+        {id: 42, password: 'password'},
+      );
+    });
+
+    it('should reject if the email has not a valid format', async () => {
+      request.input.body.email = 'invalidEmail';
+      backend.ask.withArgs('core:security:user:get')
+        .resolves({
+          id: 42,
+          username: 'foo',
+          email: 'bar',
+          role: 'user'
+        });
+
+      backend.ask.withArgs('core:security:user:verifyById')
+        .resolves(true);
+
+      await should(authController.updateMyUser(request)).be.rejectedWith(
+        BadRequestError,
+        { id: 'request:invalid:email_format' }
+      );
+
+      await should(backend.ask).be.calledWith('core:security:user:get', 42);
+      await should(backend.ask).be.calledWith(
+        'core:security:user:verifyById',
+        {id: 42, password: 'password'},
+      );
+    });
+
+    it('should reject if the username is too short', async () => {
+      request.input.body.username = 'u'; // Short username
+      backend.ask.withArgs('core:security:user:get')
+        .resolves({
+          id: 42,
+          username: 'foo',
+          email: 'bar',
+          role: 'user'
+        });
+
+      backend.ask.withArgs('core:security:user:verifyById')
+        .resolves(true);
+
+      await should(authController.updateMyUser(request)).be.rejectedWith(
+        SecurityError,
+        { id: 'security:user:username_too_short' }
+      );
+
+      await should(backend.ask).be.calledWith('core:security:user:get', 42);
+      await should(backend.ask).be.calledWith(
+        'core:security:user:verifyById',
+        {id: 42, password: 'password'},
+      );
+    });
+
+    it('should update the user informations', async () => {
+      backend.ask.withArgs('core:security:user:get')
+        .resolves({
+          id: 42,
+          username: 'foo',
+          email: 'bar',
+          role: 'user'
+        });
+
+      backend.ask.withArgs('core:security:user:verifyById')
+        .resolves(true);
+
+      backend.ask.withArgs('core:security:user:update')
+        .resolves({
+          id: 42,
+          username: 'USERNAME',
+          email: 'EMAIL@EMAIL.COM',
+          role: 'user',
+        });
+
+      const response = await authController.updateMyUser(request);
+
+      await should(backend.ask).be.calledWith('core:security:user:get', 42);
+      await should(backend.ask).be.calledWith(
+        'core:security:user:verifyById',
+        {id: 42, password: 'password'},
+      );
+      await should(backend.ask).be.calledWith(
+        'core:security:user:update',
+        42,
+        {
+          id: 42,
+          username: 'USERNAME',
+          email: 'EMAIL@EMAIL.COM',
+          role: 'user',
+        }
+      );
+
+      await should(response)
+        .be.an.Object()
+        .match({
+          id: 42,
+          username: 'USERNAME',
+          email: 'EMAIL@EMAIL.COM',
+          role: 'user',
+        });
+    });
+
+    it('should reject if cannot update user informations', async () => {
+      backend.ask.withArgs('core:security:user:get')
+        .resolves({
+          id: 42,
+          username: 'foo',
+          email: 'bar',
+          role: 'user'
+        });
+
+      backend.ask.withArgs('core:security:user:verifyById')
+        .resolves(true);
+
+      backend.ask.withArgs('core:security:user:update')
+        .resolves(null);
+
+      await should(authController.updateMyUser(request))
+        .be.rejectedWith(
+          SecurityError,
+          { id: 'security:user:update_failed' }
+        );
+
+      await should(backend.ask).be.calledWith('core:security:user:get', 42);
+      await should(backend.ask).be.calledWith(
+        'core:security:user:verifyById',
+        {id: 42, password: 'password'},
+      );
+      await should(backend.ask).be.calledWith(
+        'core:security:user:update',
+        42,
+        {
+          id: 42,
+          username: 'USERNAME',
+          email: 'EMAIL@EMAIL.COM',
+          role: 'user',
+        }
+      );
+    });
+
+    it('should reject if username is already taken', async () => {
+      backend.ask.withArgs('core:security:user:get')
+        .resolves({
+          id: 42,
+          username: 'foo',
+          email: 'bar',
+          role: 'user'
+        });
+
+      backend.ask.withArgs('core:security:user:verifyById')
+        .resolves(true);
+
+      backend.ask.withArgs('core:security:user:update')
+        .rejects({
+          code: '23505',
+          constraint: 'unique_username',
+        });
+
+      await should(authController.updateMyUser(request))
+        .be.rejectedWith(
+          SecurityError,
+          { id: 'security:user:username_taken' }
+        );
+
+      await should(backend.ask).be.calledWith('core:security:user:get', 42);
+      await should(backend.ask).be.calledWith(
+        'core:security:user:verifyById',
+        {id: 42, password: 'password'},
+      );
+      await should(backend.ask).be.calledWith(
+        'core:security:user:update',
+        42,
+        {
+          id: 42,
+          username: 'USERNAME',
+          email: 'EMAIL@EMAIL.COM',
+          role: 'user',
+        }
+      );
+    });
+
+    it('should reject if email is already taken', async () => {
+      backend.ask.withArgs('core:security:user:get')
+        .resolves({
+          id: 42,
+          username: 'foo',
+          email: 'bar',
+          role: 'user'
+        });
+
+      backend.ask.withArgs('core:security:user:verifyById')
+        .resolves(true);
+
+      backend.ask.withArgs('core:security:user:update')
+        .rejects({
+          code: '23505',
+          constraint: 'unique_email',
+        });
+
+      await should(authController.updateMyUser(request))
+        .be.rejectedWith(
+          SecurityError,
+          { id: 'security:user:email_taken' }
+        );
+
+      await should(backend.ask).be.calledWith('core:security:user:get', 42);
+      await should(backend.ask).be.calledWith(
+        'core:security:user:verifyById',
+        {id: 42, password: 'password'},
+      );
+      await should(backend.ask).be.calledWith(
+        'core:security:user:update',
+        42,
+        {
+          id: 42,
+          username: 'USERNAME',
+          email: 'EMAIL@EMAIL.COM',
+          role: 'user',
+        }
+      );
+    });
+  });
+
+  describe('#updateMyPassword', () => {
+    let request;
+    beforeEach(() => {
+      request = new Request({
+        url: '',
+        headers: {},
+        host: '',
+      });
+      request.context.user = { id: 42 };
+      request.input.body = {
+        oldPassword: 'foo',
+        newPassword: 'bar',
+      };
+    });
+
+    it('should reject if the user is not authenticated', async () => {
+      request.context.user = { id: null };
+      await should(authController.updateMyPassword(request)).be.rejectedWith(
+        SecurityError,
+        { id: 'security:user:not_authenticated' }
+      );
+    });
+
+    
+    it('should reject if the given password does not match the user\'s password', async () => {
+      backend.ask.withArgs('core:security:user:verifyById')
+        .resolves(false);
+
+      await should(authController.updateMyPassword(request)).be.rejectedWith(
+        SecurityError,
+        { id: 'security:user:invalid_credentials' }
+      );
+
+      await should(backend.ask).be.calledWith(
+        'core:security:user:verifyById',
+        {id: 42, password: 'foo'},
+      );
+    });
+
+    it('should reject if the given password is too weak', async () => {
+      authController._validatePasswordStrength = sinon.stub().returns(false);
+      backend.ask.withArgs('core:security:user:verifyById')
+        .resolves(true);
+
+      await should(authController.updateMyPassword(request)).be.rejectedWith(
+        SecurityError,
+        { id: 'security:user:password_too_weak' }
+      );
+
+      await should(backend.ask).be.calledWith(
+        'core:security:user:verifyById',
+        {id: 42, password: 'foo'},
+      );
+      await should(authController._validatePasswordStrength).be.calledWith('bar');
+    });
+
+    it('should user information if the update succeeded', async () => {
+      authController._validatePasswordStrength = sinon.stub().returns(true);
+      backend.ask.withArgs('core:security:user:verifyById')
+        .resolves(true);
+      backend.ask.withArgs('core:security:user:updatePassword')
+        .resolves({
+          id: 42,
+          username: 'foo',
+          email: 'bar',
+          role: 'user'
+        });
+
+      const response = await authController.updateMyPassword(request);
+
+      await should(backend.ask).be.calledWith(
+        'core:security:user:verifyById',
+        {id: 42, password: 'foo'},
+      );
+      await should(authController._validatePasswordStrength).be.calledWith('bar');
+      await should(backend.ask).be.calledWith('core:security:user:updatePassword', 42, 'bar');
+
+      await should(response)
+        .be.an.Object()
+        .match({
+          id: 42,
+          username: 'foo',
+          email: 'bar',
+          role: 'user'
+        });
+    });
+
+    it('should reject if update failed', async () => {
+      authController._validatePasswordStrength = sinon.stub().returns(true);
+      backend.ask.withArgs('core:security:user:verifyById')
+        .resolves(true);
+      backend.ask.withArgs('core:security:user:updatePassword')
+        .resolves(null);
+
+      await should(authController.updateMyPassword(request))
+        .be.rejectedWith(
+          SecurityError,
+          { id: 'security:user:update_failed' }
+        );
+
+      await should(backend.ask).be.calledWith(
+        'core:security:user:verifyById',
+        {id: 42, password: 'foo'},
+      );
+      await should(authController._validatePasswordStrength).be.calledWith('bar');
+      await should(backend.ask).be.calledWith('core:security:user:updatePassword', 42, 'bar');
+    });
+  });
+
+  describe('#deleteMyUser', () => {
+    let request;
+    beforeEach(() => {
+      request = new Request({
+        url: '',
+        headers: {},
+        host: '',
+      });
+      request.context.user = { id: 42 };
+    });
+
+    it('should reject if the user is not authenticated', async () => {
+      request.context.user = { id: null };
+      await should(authController.updateMyPassword(request)).be.rejectedWith(
+        SecurityError,
+        { id: 'security:user:not_authenticated' }
+      );
+    });
+
+    it('should ask core:security:user:delete with the current user id', async () => {
+      backend.ask.withArgs('core:security:user:delete')
+        .resolves(true);
+
+      const response = await authController.deleteMyUser(request);
+
+      await should(backend.ask).be.calledWith('core:security:user:delete', 42);
+
+      await should(response).be.true();
     });
   });
 });
